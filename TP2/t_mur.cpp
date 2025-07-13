c9c039a63b022cf850c1520c157688e6aeac0aa5
NOVA-STICH <maximenebili@gmail.com>
2025-07-13 17:44:56 -0400

    commentaitre ajuster dans t_mur.cpp

--- TP2/t_mur.cpp
+++ TP2/t_mur.cpp
@@ -10,13 +10,9 @@
 #include "t_mur.h"
 
 //=======================================================================================
-/* INIT_MUR
-   Cette fonction reçoit la position du mur ainsi que sa longueur.
+/* Cette fonction reçoit la position du mur ainsi que sa longueur.
    Elle déclare et initialise une variable de type « t_mur » avec les deux valeurs
-   reçues et retourne ce nouveau mur ainsi créé.
-   PARAMÈTRES : pos_mur - position en X du mur (int)
-                longueur - longueur du mur en Y (int)
-   RETOUR : structure t_mur initialisée */
+   reçues et retourne ce nouveau mur ainsi créé. */
 t_mur init_mur(int pos_mur, int longueur) {
     t_mur mur = { 0 }; // Initialise tous les paramètres à zéro
     mur.longueur = longueur;
@@ -25,35 +21,26 @@ t_mur init_mur(int pos_mur, int longueur) {
 }
 
 //=======================================================================================
-/* CONTACT_MUR
-   Reçoit un mur par référence et la position X d'une personne.
+/* Reçoit un mur par référence et la position X d'une personne.
    Vérifie si cette position est à moins de DIST_MUR pixels (distance absolue)
-   et retourne true si c'est le cas, false sinon.
-   PARAMÈTRES : *mur - pointeur sur le mur (const t_mur*)
-                pos_personne - position X à vérifier (double)
-   RETOUR : booléen indiquant si contact avec le mur */
+   et retourne true si c'est le cas, false sinon. */
 bool contact_mur(const t_mur* mur, double pos_personne) {
     return (fabs(mur->pos_mur - pos_personne) < DIST_MUR);
 }
 
 //=======================================================================================
-/* MUR_ACTIF
-   Reçoit un mur par référence et incrémente le nombre de jours de confinement.
+/* Reçoit un mur par référence et incrémente le nombre de jours de confinement.
    Si ce nombre atteint NB_JOURS_MAX, retourne false (mur désactivé).
-   Sinon retourne true (mur toujours actif).
-   PARAMÈTRES : *mur - pointeur sur le mur (t_mur*)
-   RETOUR : booléen indiquant l'état actif du mur */
+   Sinon retourne true (mur toujours actif). */
 bool mur_actif(t_mur* mur) {
     mur->nb_jours++;
     return (mur->nb_jours < NB_JOURS_MAX);
 }
 
 //=======================================================================================
-/* DESSINER_MUR
-   Reçoit un mur par référence et le dessine à l'écran
+/* Reçoit un mur par référence et le dessine à l'écran 
    (pour le mode avec affichage graphique).
-   Utilise la fonction afficher_mur() du module utilitaires_affichage.
-   PARAMÈTRES : *mur - pointeur sur le mur (const t_mur*) */
+   Utilise la fonction afficher_mur() du module utilitaires_affichage. */
 void dessiner_mur(const t_mur* mur) {
     afficher_mur(mur->pos_mur, mur->longueur);
 }
